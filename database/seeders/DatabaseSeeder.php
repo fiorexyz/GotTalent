cd94acbd163d079df43b1a302714f3e019d6176c
contreraskarla <116602185+contreraskarla@users.noreply.github.com>
2022-10-25 10:21:27 -0600

    creacion de usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,87 +17,182 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Karla Daniela Contreras Rodas',
+            'email' => 'karla.contreras2023@poma.superate.org.sv',
+            'age' => 17,
+            'phone' => '69578801',
+            'superate' => 'Fundacion Poma',
+            'sub_category_id' => 5,
+            'timePracticing' => 4,
+            'about' => 'I do drawing because when I was a child I discovered that it was my 
+            safe place and it makes me happy.'
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Cecilia del Carmen Villalta Amaya',
+            'email' => 'cecilia.villalta2023@poma.superate.org.sv',
+            'age' => 16,
+            'phone' => '74945003',
+            'superate' => 'Fundacion Poma',
+            'sub_category_id' => 13,
+            'timePracticing' => 2,
+            'about' => 'I really enjoy dancing because is when I forget all the bad stuff and
+             concentrate on enjoying the moment. When I make mistakes I appreciate
+             learning from my mistakes so I can improve more and more.'
         ]);
 
+        User::factory()->create([
+            'name' => 'Angie Fiorella Alberto Guzman',
+            'email' => 'angie.alberto2023@poma.superate.org.sv',
+            'age' => 16,
+            'phone' => '79014868',
+            'superate' => 'Fundacion Poma',
+            'sub_category_id' => 15,
+            'timePracticing' => 2,
+            'about' => 'I learnt baking with my grandmother'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Ana Ester Salazar Mendoza',
+            'email' => 'ana.salazar2023@poma.superate.org.sv',
+            'age' => 17,
+            'phone' => '79546348',
+            'superate' => 'Fundacion Poma',
+            'sub_category_id' => 12,
+            'timePracticing' => 3,
+            'about' => 'I like to play the piano when I am bored or when I am really stressed'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Reynaldo Jose Juarez ',
+            'email' => 'reynaldo.juarez2023@poma.superate.org.sv',
+            'age' => 16,
+            'phone' => '74309748',
+            'superate' => 'Fundacion Poma',
+            'sub_category_id' => 6,
+            'timePracticing' => 2,
+            'about' => 'When I am singing I feel like I am living'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Sofia Franchesca Perez',
+            'email' => 'sofia.perez2023@poma.superate.org.sv',
+            'age' => 16,
+            'phone' => '74947514',
+            'superate' => 'Fundacion Poma',
+            'sub_category_id' => 14,
+            'timePracticing' => 1,
+            'about' => 'I love cooking because I can be creative on that'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Jacqueline Vanessa Mendoza ',
+            'email' => 'jacqueline.mendoza2023@poma.superate.org.sv',
+            'age' => 16,
+            'phone' => '72268742',
+            'superate' => 'Fundacion Poma',
+            'sub_category_id' => 2,
+            'timePracticing' => 3,
+            'about' => 'I practice this sport because it helps me to have a better concentration
+            and body movement.'
+        ]);
+        
+        //ID: 1
         Category::factory()->create([
-            'type' => 'Deportivo'
+            'type' => 'Sports'
         ]);
 
+        //ID: 2
         Category::factory()->create([
-            'type' => 'Arte'
+            'type' => 'Art'
         ]);
 
+        //ID: 3
         Category::factory()->create([
-            'type' => 'Musica'
+            'type' => 'Music'
         ]);
 
+        //ID: 4
         Category::factory()->create([
-            'type' => 'Gastronomia'
+            'type' => 'Gastronomy'
         ]);
 
+        //ID: 1
         SubCategory::factory()->create([
             'category_id' => 1,
-            'type' =>'Futbol'
+            'type' =>'Soccer'
         ]);
-
+        //ID 2
         SubCategory::factory()->create([
             'category_id' => 1,
             'type' => 'Basketball'
         ]);
-
+        //ID 3
         SubCategory::factory()->create([
             'category_id' => 1,
             'type' => 'Volleyball'
         ]);
-
+        //ID 4
         SubCategory::factory()->create([
             'category_id' => 2,
-            'type' => 'Pintura'
+            'type' => 'Painting'
+        ]);
+        //ID 5
+        SubCategory::factory()->create([
+            'category_id' => 2,
+            'type' => 'Drawing'
+        ]);
+        //ID 6
+        SubCategory::factory()->create([
+            'category_id' => 2,
+            'type' => 'Singing'
         ]);
 
+        //ID 7
         SubCategory::factory()->create([
             'category_id' => 2,
-            'type' => 'Dibujo'
+            'type' => 'Fotography'
         ]);
-
+        //ID 8
         SubCategory::factory()->create([
             'category_id' => 2,
-            'type' => 'Canto'
+            'type' => 'Poetry'
         ]);
-
+        //ID 9
         SubCategory::factory()->create([
             'category_id' => 2,
-            'type' => 'Instrumento'
+            'type' => 'Performance'
         ]);
-
+        //ID 10
         SubCategory::factory()->create([
             'category_id' => 2,
-            'type' => 'Fotografia'
+            'type' => 'Crochet'
         ]);
-
+        //ID 11
         SubCategory::factory()->create([
-            'category_id' => 2,
-            'type' => 'Poesia'
+            'category_id' => 3,
+            'type' => 'Singing'
         ]);
-
+        //ID 12
         SubCategory::factory()->create([
-            'category_id' => 2,
-            'type' => 'Actuacion'
+            'category_id' => 3,
+            'type' => 'Play an instrument'
         ]);
-
+        //ID 13
         SubCategory::factory()->create([
-            'category_id' => 2,
-            'type' => 'Croche'
+            'category_id' => 3,
+            'type' => 'Dancing'
         ]);
-
+        //ID 14
         SubCategory::factory()->create([
-            'category_id' => 2,
-            'type' => 'Baile'
+            'category_id' => 4,
+            'type' => 'Cooking'
+        ]);
+        //ID 15
+        SubCategory::factory()->create([
+            'category_id' => 4,
+            'type' => 'Baking'
         ]);
     }
 }
