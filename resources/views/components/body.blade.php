b681f7ece7b2a03958e4336a859c058cdb6c646b
Haruki1707 <diego.fernandogc@gmail.com>
2022-11-02 11:49:54 -0600

    css v=1

--- resources/views/components/body.blade.php
+++ resources/views/components/body.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=1">
     <link rel="shortcut icon" href="favicon.ico" type="image/png">
     <title>Document</title>
 </head>
